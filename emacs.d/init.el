4dcaddc61d405a8dbdd459ddfa4aee9c7e0634a2
Oscar Rendon <orendon@gmail.com>
2016-05-14 08:13:05 -0500

    Add ruby end git emacs packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,14 @@
+;; packages
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
 
 ;; hide emacs toolbar
 (tool-bar-mode -1)
 
-;; Clojure
+;; clojure
 (unless (package-installed-p 'clojure-mode)
   (package-install 'clojure-mode))
 (unless (package-installed-p 'cider)
@@ -15,3 +17,16 @@
   (package-install 'paredit))
 
 (add-hook 'clojure-mode-hook #'paredit-mode)
+
+;; ruby
+(unless (package-installed-p 'robe)
+  (package-install 'robe))
+(unless (package-installed-p 'enh-ruby-mode)
+  (package-install 'enh-ruby-mode))
+
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+
+;; git
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+ 
