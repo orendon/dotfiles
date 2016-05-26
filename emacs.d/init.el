6fe4f3f8a81f33178ae3e0dae008a7eadc6e1d40
Oscar Rendon <orendon@gmail.com>
2016-05-26 11:43:50 -0500

    Autoload enhanced ruby mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -26,6 +26,9 @@
 (unless (package-installed-p 'enh-ruby-mode)
   (package-install 'enh-ruby-mode))
 
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
 
 ;; git
